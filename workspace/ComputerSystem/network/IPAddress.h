2fc73dd02d1ae41f0d909c402f9c6b095e4e3fde
Han Wang <wangh0402@gmail.com>
2011-10-11 01:36:38 +0800

    add network folder and add ip address converter

---
+++ workspace/ComputerSystem/network/IPAddress.h
@@ -0,0 +1,14 @@
+/*
+ * IPAddress.h
+ *
+ *  Created on: 2011-10-11
+ *      Author: wanghan
+ */
+
+#ifndef IPADDRESS_H_
+#define IPADDRESS_H_
+
+char * hex2dd(char * addr);
+unsigned int dd2hex(char * addr);
+
+#endif /* IPADDRESS_H_ */
