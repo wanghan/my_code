5c6679dc6ef621e899d812e9e146b872218ca8c8
Han Wang <hanwang.work@gmail.com>
2012-08-09 12:17:52 +0800

    revert all sdo

--- powershell/MyToolkit/MyToolkit.psm1
+++ powershell/MyToolkit/MyToolkit.psm1
@@ -1,2 +1,3 @@
 ﻿. $psScriptRoot\Update-Git.ps1
 . $psScriptRoot\Copy-Folder.ps1
+. $psScriptRoot\Redo-SDO.ps1
---
+++ powershell/MyToolkit/Redo-SDO.ps1
@@ -0,0 +1,34 @@
+﻿function Redo-SDO{
+<#
+
+.SYNOPSIS
+
+
+.EXAMPLE
+
+
+.EXAMPLE
+
+
+#>
+
+    begin {
+        Set-StrictMode -Version Latest
+    }
+    process {
+        $a = sd opened
+
+        if($a){
+
+            $lines = $a.split("`t`n")
+
+            foreach($line in $lines){
+                $newline = $line.Substring($line.IndexOf("sources"))
+                $newline = ($newline -split '#')[0]
+                sd revert $newline 
+            }
+        }
+        exit
+    }
+
+}
